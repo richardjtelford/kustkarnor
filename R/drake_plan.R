5e4da175a3d9425d3daa58d6ba2cdddb89c3a95e
richardjtelford <richard.telford@bio.uib.no>
2019-09-16 15:40:53 +0200

    merge and calculate percent

--- R/drake_plan.R
+++ R/drake_plan.R
@@ -1,39 +1,49 @@
 library("drake")
 library("tidyverse")
 library("assertr")
-library("readxl")
+#library("readxl")
 library("vegan")
 library("rioja")
 library("palaeoSig")
-library("Hmisc") # assumes you have mdb-tools installed
+#library("Hmisc") # assumes you have mdb-tools installed
 
 plan <- drake_plan(
   #load calibration data
 
   #load fossil data
-  fos0 = read_xlsx("data/Alla kustkärnor med koder_20190903.xlsx", sheet = "Sheet1", skip = 1) %>%
+  fos0 = readxl::read_xlsx("data/Alla kustkärnor med koder_20190903.xlsx", sheet = "Sheet1", skip = 1) %>%
     rename(site = ...1, sample = ...2, depth = ...3, date = ...4, countsum = ...5),
   fos_meta = fos0 %>% select(site, sample, depth, date, countsum),
 
   #calculate percent
   fos_percent = fos0 %>%
-    verify(all.equal(#check countsum matches actual countsums
+    #check countsum matches actual countsums
+    verify(all.equal(
       countsum,
       fos0 %>%
         select(-site,-sample,-depth,-date,-countsum) %>%
         rowSums()
     )) %>%
-    select(-site,-sample,-depth,-date) %>%
-    mutate_at(vars(-countsum), ~ {
-      . / countsum * 100
-    }) %>%
-    select(-countsum)
+    select(-countsum) %>%
+    #merge taxa
+    gather(key = taxa, value = count, -site, -sample,-depth,-date) %>%
+    mutate(taxa = gsub("\\.\\.\\.\\d*$", "", taxa)) %>% #dups end with three dots and then digits
+    group_by(site, sample, depth, date, taxa) %>%
+    summarise(count = sum(count)) %>%
+    #calculate percent
+    mutate(percent = count/sum(count) * 100) %>%
+    select(-count) %>%
+    spread(key = taxa, value = percent) %>%
+    #remove metadata
+    ungroup() %>%
+    select(-site,-sample,-depth,-date),
 
   #diagnostics
 
   #reconstructions
 
   #rmarkdown
+  output = rmarkdown::render(knitr_in("kustkarnor.Rmd"))
 )
 
 config <- drake_config(plan = plan)
